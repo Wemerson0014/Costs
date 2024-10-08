3be4f44d08fc58211aeeb8a6f147708506be0208
Wemerson Fernandes <wemersonf.rangel@outlook.com>
2024-10-08 18:27:44 -0300

    Componente de loading
    Cria componente de loading para carregamento da página de listagem de projetos

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 
 import Message from '../layout/Message'
 import Container from '../layout/Container'
+import Loading from '../layout/Loading'
 import LinkButton from '../layout/LinkButton'
 import ProjectCard from '../project/ProjectCard'
 
@@ -11,6 +12,7 @@ import styles from './Projects.module.css'
 function Projects() {
 
     const [projects, setProjects] = useState([])
+    const [removeloading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
     let message = ''
@@ -19,17 +21,20 @@ function Projects() {
     }
 
     useEffect(() => {
-        fetch('http://localhost:5000/projects', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then((resp) => resp.json())
-        .then((data) => {
-            setProjects(data)
-        })
-        .catch((err) => console.log(err))
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProjects(data)
+                setRemoveLoading(true)
+            })
+            .catch((err) => console.log(err))
+        }, 300)
     }, [])
 
     return (
@@ -50,6 +55,10 @@ function Projects() {
                         key={project.id}
                          />
                        ))}
+                {!removeloading && <Loading />}
+                {removeloading && projects.length === 0 (
+                    <p>Não há projetos cadastrados</p>
+                )}
             </Container>
         </div>
     )
